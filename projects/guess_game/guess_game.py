518ee5b1265bba304f7ac9d9e2820099cb87f2cf
Egor Fedorov <egor.fedorov@gmail.com>
2019-04-07 15:16:52 +0300

    minors

--- projects/guess_game/guess_game.py
+++ projects/guess_game/guess_game.py
@@ -11,8 +11,9 @@ while True:
         break
     if guess == str(picked):
         correct_hits += 1
-        print("CORRECT! You make a good shot!")
+        print("CORRECT! You made a good shot!")
     else:
         wrong_hits += 1
         print(f"WRONG! It was '{picked}' not '{guess}' Try again")
-print(f"Your hits score {round(correct_hits / (correct_hits + wrong_hits) * 100, 1)}%. Bye!")
+correct_rate = correct_hits / (correct_hits + wrong_hits)
+print(f"Your hits score {round(correct_rate * 100, 1)}%. Bye!")
