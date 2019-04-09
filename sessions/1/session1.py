b44ad75f3e0a6b5f8202e6bede8af262ae8af1ae
Egor Fedorov <egor.fedorov@gmail.com>
2019-04-09 10:11:37 +0300

    better cases for session 1

--- sessions/1/session1.py
+++ sessions/1/session1.py
@@ -1,50 +1,101 @@
 # comment
 
-print("We can print")
-5 / 2
-5 // 2
-5 % 2
-1.1 + 2
-2**16
+print("Hello world")
 
+# if (length("string") > 0) {
+#     int a = 0;
+#     print("something");
+# }
+if "string":
+    a = 0
+    print("something")
+
+print("====================================")
+
+print(5 / 2)
+print(5 // 2)
+print(5 % 2)
+print(1.1 + 2)
+print(2**16)
+
+print("====================================")
 
 variable = "string value"
+variable += " add string"
 variable = 0
+variable += 1
 variable = None
 variable = True
 variable = True if 6 > 3 else False
+print(type(variable))
+
+print("====================================")
 
 "string in double \t (tab) quotes \n new line"
 'string in single quotes \n \t the same'
-f'formatted string can contains variables {variable} or expressions {5+2}' # since 3.6
-'formatted string can contains variables {variable} or expressions '.format(variable=variable) # sinse 3.0
-c = "somestring"[4]
-c = "somestring"[2:4]
-c = "somestring"[::2]
-c = "somestring"[::-1]
-len(c)
-c = "ab" + "edc"[::-1]
 
-variable = True or False
-variable = not variable
+print("concatenate " + 'strings')
+print("but only strings! " + str(True) + ' ' + str(18))
+
+print(f'since 3.6 f-strings expand variables {variable} or expressions {5+2}')
+print('since 3.0 format function of string {variable}'.format(variable=variable))
+print('or by positions {} {}'.format("position1", 12+8))
+print('old-school formattings %s %04d %.2f' % ("string", 18, 3.1415))
+
+print("subrange"[4])
+print("subrange"[2:4])
+print("subrange"[::2])
+print("reverse"[::-1])
+print("ab" + "edc"[::-1])
+print(len("string"))
+
+print("====================================")
+
+print(True or False)
 variable = None
-variable is None
+print(not False)
+print(variable is None)
+print(variable is False)
+print(not True and not False)
 
-# we can compare
-"0" == 0 # False
-bool("") # False
+print(bool(0))
+print(bool(""))
+print(bool([]))
 
-variable = not True and not False
-if variable:
-    print("YES")
+print("====================================")
+
+# we can compare different types
+print("0" == 0) # False
+print(0 == False) # True
+print(0 is False) # False
+
+print("====================================")
+
+tuple = ("some", 1, False, 18)
+print(tuple)
+
+print("====================================")
+
+variable = "18"
+if variable == 18:
+    print("yes int 18")
+elif variable == False:
+    print("yes False")
 else:
-    print("No")
+    print("Other case")
 
-for i in range(1,3):
-    print(f"for {i}")
+for i in (1, 2, 3, 4):
+    print(f"for over tuple {i}")
+
+for i in range(0,3):
+    print(f"for in range {i}")
+
+i = 0
+while i < 2:
+    print(f"In while i={i}")
+    i += 1
 
 import random
-
 while True:
     value = random.randint(0, 4)
     print(f"variable is {value}")
@@ -54,9 +105,13 @@ while True:
 while False:
     pass
 else:
-    print("While loop stop")
+    print("While loop finished")
 
-print("After while")
+# No DO-WHILE
+# No SWITCH-CASE
+
+print("====================================")
+
 
 # now let's go to projects/guess_game
 # good place to continue https://learnxinyminutes.com/docs/python3/
