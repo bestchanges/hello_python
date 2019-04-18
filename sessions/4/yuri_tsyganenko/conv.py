96d8ac1bb4487b68ad09a4dd042a01418917707d
Yuri Tsyganenko <yuri.tsyganenko@dataart.com>
2019-04-18 08:17:15 +0100

    Save, load and some tuning

--- sessions/4/yuri_tsyganenko/conv.py
+++ sessions/4/yuri_tsyganenko/conv.py
@@ -5,14 +5,6 @@
 from rates import Rates
 
 
-def set_rate(line):
-    parts = line.split('=')
-    val = parts[1].strip()
-    curr = parts[0].split('/')
-    curr_from = curr[0].strip()
-    curr_to = curr[1].strip()
-    rates.set(curr_from, curr_to, val)
-
 def print_usage():
     print("""Usage:
     value currency                          : conversion,  e.g.: 10 USD
@@ -23,26 +15,22 @@ def print_usage():
 def main_loop():
     print_usage()
     cmd = ""
-    while cmd.strip() != 'q':
+    while cmd.strip() not in ['q', 'Q']:
         print("> ")
         cmd = input()
         args = cmd.strip().split()
         if len(args) == 2:  # assuming it is, e.g. 10 USD
             rates.calc(args[0], args[1])
         elif '=' in cmd:
-            set_rate(cmd)
+            rates.parseRate(cmd)
         else:
             print_usage()
+    rates.save()
 
 
 # ========  Execution  =============
 
-rates = Rates()
-
-defaultRates = " USD/RUR=64.35 BTC/RUR=325960 USD/EUR=0.88 EUR/RUR=72.72"
-
-for line in defaultRates.split():
-    set_rate(line)
+rates = Rates(" USD/RUR=64.35 BTC/RUR=325960 USD/EUR=0.88 EUR/RUR=72.72")
 
 main_loop()
 
--- sessions/4/yuri_tsyganenko/rates.py
+++ sessions/4/yuri_tsyganenko/rates.py
@@ -1,29 +1,57 @@
 #!/usr/bin/env python3
 
+import json
+
 # методы для вычислений обмена и данные для этого.
 
 
 class Rates:
     """ calculate rates and handle needed data """
+    filename = 'rates.json'
 
-    def __init__(self):
+    # def __init__(self):
+    #     self.rate = {}
+    #     self.load()
+
+    def __init__(self, initial_rates):
         self.rate = {}
+        if not self.load():
+            for line in initial_rates.split():
+                self.parseRate(line)
 
-    # Todo implement save-load
-    # def load(filename):
-    # def save(filename):
+
+    def load(self):
+        try:
+            with open(Rates.filename, 'rt') as file:
+                self.rate = json.load(file)
+            return True
+        except FileNotFoundError:  # as fnf:
+            return False
+
+    def save(self):
+        print("Bye!")
+        with open(Rates.filename, 'wt') as file:
+            json.dump(self.rate, file)
 
     def set(self, curr_from, curr_to, value):
-        # todo: any validation? trim?
         # init - in case it is the first assignment
-        self.rate.setdefault(curr_from, {})
-        self.rate.setdefault(curr_to, {})
-        self.rate[curr_from].setdefault(curr_to, {})
-        self.rate[curr_to].setdefault(curr_from, {})
+        cto = curr_to.strip()
+        cfrom = curr_from.strip()
+        self.rate.setdefault(cfrom, {})
+        self.rate.setdefault(cto, {})
+        self.rate[cfrom].setdefault(cto, {})
+        self.rate[cto].setdefault(cfrom, {})
         # assign
-        self.rate[curr_from][curr_to] = float(value)
-        self.rate[curr_to][curr_from] = 1.0 / float(value)
+        self.rate[cfrom][cto] = float(value)
+        self.rate[cto][cfrom] = 1.0 / float(value)
 
+    def parseRate(self, line):
+        parts = line.split('=')
+        val = parts[1].strip()
+        curr = parts[0].split('/')
+        curr_from = curr[0].strip()
+        curr_to = curr[1].strip()
+        self.set(curr_from, curr_to, val)
 
     def get(self, curr_from, curr_to):
         return self.rate[curr_from][curr_to]
@@ -43,5 +71,11 @@ class Rates:
         for curr_to, perUnit in self.rate[curr_from].items():
             print("{0:,.02f} {1:}".format(float(amount) * perUnit, curr_to))
 
-
-
+#              Error(s):
+# >>> u=64.35
+# >>> am = "10"
+# >>> print("{0:,.02f} {}".format(float(am) * u, 'RUR'))
+# Traceback (most recent call last):
+#   File "<stdin>", line 1, in <module>
+# ValueError: cannot switch from manual field specification to automatic field numbering
+#
