0499bf7ccddcb8f7499c896adc830ab59d0dd0b2
Yuri Tsyganenko <yuri.tsyganenko@dataart.com>
2019-05-30 14:14:04 +0100

    more questions (playing with fix by Egor)

--- sessions/12/yuri_tsyganenko/replace_buildin.py
+++ sessions/12/yuri_tsyganenko/replace_buildin.py
@@ -8,10 +8,10 @@ class Replacer:
         #  Replace system method by inadequate one
         # assert (self.original is None)
         self.original = builtins.print
-        def fake_print(*args):
-            pass
+        # def fake_print(*args):
+        #     pass  # exit(0)
         # builtins.print = exit   # No printing allowed. Use logging instead
-        builtins.print = fake_print   # No printing allowed. Use logging instead
+        builtins.print = builtins.exit   # No printing allowed. Use logging instead
 
     def __exit__(self, comment, *args):
         # replace it back
@@ -26,7 +26,9 @@ class Replacer:
 if __name__ == "__main__":
     print("Hello")
 
-    with Replacer() as r:   # AttributeError: __enter__
-        print("Can you see it?")
+    with Replacer() as r:
+        print("Can you see it?")  # Why this is still printed (in red - as stderr)! - due to incorrect param?
+        print(0)       # just exit.   #  Why "Process finished with exit code 1"   Expected exit code 0
+        print("What about now - do you see it?")
 
     print("Bye")  # Ok to print here
