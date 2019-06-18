fea9103689f8dbd2af13ff2a5ac125706c8c1c8a
Egor Fedorov <egor.fedorov@gmail.com>
2019-06-18 11:29:40 +0300

    djange session 2

--- django/studyworthy/app/urls.py
+++ django/studyworthy/app/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('courses/<int:course_id>', views.course_info, name='course_info'),
 ]
--- django/studyworthy/app/views.py
+++ django/studyworthy/app/views.py
@@ -3,6 +3,13 @@ from django.shortcuts import render
 # Create your views here.
 from django.http import HttpResponse
 
+from app.models import Course
+
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+def course_info(request, course_id):
+    course = Course.objects.get(id=course_id)
+    return HttpResponse(f"course_info {course.title}")
