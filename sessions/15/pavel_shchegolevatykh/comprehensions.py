f81c8ea1f73b424dcb2d82ba603af94eb261c97f
Pavel Shchegolevatykh <pshchegolevatykh@hotmail.com>
2019-06-24 23:11:08 +0300

    Fix PR comments for session15

--- sessions/15/pavel_shchegolevatykh/comprehensions.py
+++ sessions/15/pavel_shchegolevatykh/comprehensions.py
@@ -8,13 +8,13 @@ print(f'Dict via zip function: {months_dict}')
 more_than_30_days = {name for name, days in months_dict.items() if days > 30}
 print(f'More than 30 days months: {more_than_30_days}')
 
-odd_months = [(name, days) for idx, (name, days) in enumerate(months_dict.items(), 1) if idx % 2 == 0]
-print(f'Odd months: {odd_months}')
+even_months = tuple(name for idx, (name, days) in enumerate(months_dict.items(), 1) if idx % 2 == 0)
+print(f'Even months: {even_months}')
 
-even_months_generator = (name for name, days in months_dict.items() if days % 2 != 0)
-print(f'First even days month: {next(even_months_generator)}')
-print(f'Second even days month: {next(even_months_generator)}')
-print(f'Third even days month: {next(even_months_generator)}')
+odd_months_generator = (name for name, days in months_dict.items() if days % 2 != 0)
+print(f'First odd days month: {next(odd_months_generator)}')
+print(f'Second odd days month: {next(odd_months_generator)}')
+print(f'Third odd days month: {next(odd_months_generator)}')
 
 no_i_in_names = [name for name, days in months_dict.items() if 'И' not in name]
 print(f'No i in month names: {no_i_in_names}')
